3a8870237eec6ecc29131af365847830f6170b0c
WeimarJr <weimarvlademirojunior@gmail.com>
2025-04-24 18:36:53 -0400

    primeira commit

---
+++ src/main/java/com/WeimarJr/ApiDeNotasETarefas/ApiCrudDeNotasETarefas/ApiCrudDeNotasETarefasApplication.java
@@ -0,0 +1,13 @@
+package com.WeimarJr.ApiDeNotasETarefas.ApiCrudDeNotasETarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudDeNotasETarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudDeNotasETarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WeimarJr/ApiDeNotasETarefas/ApiCrudDeNotasETarefas/ApiCrudDeNotasETarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WeimarJr.ApiDeNotasETarefas.ApiCrudDeNotasETarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudDeNotasETarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
